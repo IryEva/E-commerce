e1dd93dbb0c0691ccd21f6214d7391e8f233cc9a
Iry Eva <nivedithaharidas333@gmail.com>
2023-10-26 23:09:49 +0530

    task 1 ecommerce mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,31 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete','root','nivya@333', {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize; 
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://nivedithamh3:niveditha@cluster0.dvig9is.mongodb.net/?retryWrites=true&w=majority'
+       )
+        .then(client => {
+            console.log('Connected!');
+            _db = client.db('test');
+            callback();
+        })
+        .catch(err => {
+        console.log(err);
+        throw err;
+    });
+};
+
+const getDb = () => {
+    if(_db){
+        return _db;
+    }
+    throw 'No database found';
+};
+
+exports.mongoConnect=mongoConnect;
+exports.getDb = getDb;
+
+
